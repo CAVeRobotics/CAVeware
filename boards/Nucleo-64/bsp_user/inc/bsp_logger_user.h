cf601dc9954db324424bcc92ed8cef378ea8a1c9
Benjamin Esquieres <40147170+delta-12@users.noreply.github.com>
2025-09-04 17:54:04 -0400

    CVW-59 remove unused boards (#15)

--- boards/Nucleo-64/bsp_user/inc/bsp_logger_user.h
+++
@@ -1,8 +0,0 @@
-#ifndef BSP_LOGGER_USER_H
-#define BSP_LOGGER_USER_H
-
-#include "bsp.h"
-
-void BspLoggerUser_RegisterCustomLogger(void);
-
-#endif /* BSP_LOGGER_USER_H */
--- boards/STM32-E407/bsp_user/src/bsp_tick_user.c
+++
@@ -1,7 +0,0 @@
-#include "bsp_tick_user.h"
-
-#include "tim.h"
-
-#include "bsp.h"
-
-Bsp_TimerHandle_t *const BspTickUser_TimerHandle = &htim6;
