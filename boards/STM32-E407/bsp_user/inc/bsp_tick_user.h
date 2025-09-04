dd5d05a498bfa001bc5cbabf823115d05c404961
delta-12 <btesq235@gmail.com>
2025-09-04 17:38:23 -0400

    CVW-59 remove unused boards

--- boards/STM32-E407/bsp_user/inc/bsp_tick_user.h
+++
@@ -1,8 +0,0 @@
-#ifndef BSP_TICK_USER_H
-#define BSP_TICK_USER_H
-
-#include "bsp.h"
-
-extern Bsp_TimerHandle_t *const BspTickUser_TimerHandle;
-
-#endif /* BSP_TICK_USER_H */
--- boards/STM32-E407/bsp_user/inc/bsp_user.h
+++
@@ -1,6 +0,0 @@
-#ifndef BSP_USER_H
-#define BSP_USER_H
-
-void BspUser_Initialize(void);
-
-#endif /* BSP_USER_H */
