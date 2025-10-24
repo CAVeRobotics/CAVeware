6bb61b4ee4283a379fa9164185815844892f3359
Benjamin Esquieres <40147170+delta-12@users.noreply.github.com>
2025-10-24 08:53:21 -0700

    CVW-41 differential 4 wheel drive (#17)
    * CVW-41 add caveboard-mini status LED
    
    * CVW-41 add compile definitions for supported bots
    
    * CVW-41 apply -Werror to Bots sources when compiled
    
    * CVW-41 update user config for testing
    
    * CVW-41 open loop 4wd
    
    * CVW-41 update rover 4ws to match new cavebot interface

--- bots/inc/rover_4ws.h
+++ bots/inc/rover_4ws.h
@@ -19,16 +19,14 @@ Cavebot_Error_t Rover4ws_ConfigureMotor(const CavebotUser_Motor_t motor,
 Cavebot_Error_t Rover4ws_ConfigureMotorPid(const CavebotUser_Motor_t motor, const double kp, const double ki, const double kd);
 Cavebot_Error_t Rover4ws_ConfigureSteeringPid(const double kp, const double ki, const double kd);
 Cavebot_Error_t Rover4ws_ConfigureEncoder(const CavebotUser_Motor_t motor, const double smoothing_factor);
-Cavebot_Error_t Rover4ws_EnableSteering(void);
-Cavebot_Error_t Rover4ws_DisableSteering(void);
-Cavebot_Error_t Rover4ws_StartMotors(void);
-Cavebot_Error_t Rover4ws_StopMotors(void);
 Cavebot_Error_t Rover4ws_EnableEncoders(void);
 Cavebot_Error_t Rover4ws_SampleEncoders(void);
 Cavebot_Error_t Rover4ws_EnableSpeedControl(void);
 Cavebot_Error_t Rover4ws_DisableSpeedControl(void);
 Cavebot_Error_t Rover4ws_EnableSteeringControl(void);
 Cavebot_Error_t Rover4ws_DisableSteeringControl(void);
+Cavebot_Error_t Rover4ws_Arm(void);
+Cavebot_Error_t Rover4ws_Disarm(void);
 Cavebot_Error_t Rover4ws_Task(void);
 Cavebot_Error_t Rover4ws_Drive(const Bsp_MetersPerSecond_t speed, const Bsp_RadiansPerSecond_t turn_rate);
 Cavebot_Error_t Rover4ws_ErrorCheck(const Cavebot_Error_t error_0,
