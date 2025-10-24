c40c77375ffb096ab6a52b5221c74d3c8b3b7f18
delta-12 <btesq235@gmail.com>
2025-10-24 17:34:32 -0400

    CVW-63 enable tim7 on CAVeBoard Mini for use with stepper

--- boards/CAVeBoard-Mini/bsp_user/src/bsp_timer_user.c
+++ boards/CAVeBoard-Mini/bsp_user/src/bsp_timer_user.c
@@ -1,11 +1,18 @@
 #include "bsp_timer_user.h"
 
+#include <stdbool.h>
+
 #include "tim.h"
 
 Bsp_Timer_t BspTimerUser_HandleTable[BSP_TIMER_USER_TIMER_MAX] = {
     [BSP_TIMER_USER_TIMER_0] = {
-        /* TODO */
-        0,
+        .timer_handle            = &htim7,
+        .counts_elapsed          = 0U,
+        .counts_elapsed_shadow   = 0U,
+        .counts_offset           = 0U,
+        .sampling                = false,
+        .period_elapsed_callback = NULL,
+        .counts                  = 0U,
     },
 };
 
