213b523e1594cc59691a404de74c0269e58afe45
delta-12 <btesq235@gmail.com>
2025-11-13 13:19:35 -0500

    CVW-56 add ticket numbers to TODOs

--- boards/CAVeBoard-Mini/bsp_user/src/bsp_spi_user.c
+++ boards/CAVeBoard-Mini/bsp_user/src/bsp_spi_user.c
@@ -21,7 +21,7 @@ Bsp_Spi_t *BspSpiUser_GetSpi(const Bsp_SpiHandle_t *const spi_handle)
 {
     Bsp_Spi_t *spi = NULL;
 
-    /* TODO duplicate this pattern to the rest of the BSP User "get" methods */
+    /* TODO CVW-76 duplicate this pattern to the rest of the BSP User "get" methods */
     for (BspSpiUser_Spi_t user_spi = BSP_SPI_USER_0; user_spi < BSP_SPI_USER_MAX; user_spi++)
     {
         if (spi_handle == BspSpiUser_HandleTable[user_spi].spi_handle)
