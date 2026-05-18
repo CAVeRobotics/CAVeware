872fce91d48583ad1b9756946b1cea11e9476f9f
delta-12 <btesq235@gmail.com>
2026-05-18 09:10:30 -0400

    CVW-50 WIP: fsm

---
+++ cavebot/inc/fsm.h
@@ -0,0 +1,25 @@
+#ifndef FSM_H
+#define FSM_H
+
+#include <stdbool.h>
+
+typedef struct Fsm_State Fsm_State_t;
+
+struct Fsm_State
+{
+    char *name;
+    void (*enter)(void);
+    void (*exit)(void);
+    Fsm_State_t *(*update)(void);
+};
+
+typedef struct
+{
+    char *name;
+    Fsm_State_t *state;
+} Fsm_t;
+
+bool Fsm_Initialize(Fsm_t *const fsm, const Fsm_State_t *const state, const char *const name);
+void Fsm_Update(Fsm_t *const fsm);
+
+#endif /* FSM_H */
